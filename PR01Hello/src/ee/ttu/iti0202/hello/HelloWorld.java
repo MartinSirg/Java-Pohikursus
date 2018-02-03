f9568ff993247b2f1677ddd6e7e98ccbae411276
Martin <masirg@ttu.ee>
2018-02-03 19:58:26 +0200

    v 0.1

--- PR01Hello/src/ee/ttu/iti0202/hello/HelloWorld.java
+++ PR01Hello/src/ee/ttu/iti0202/hello/HelloWorld.java
@@ -10,11 +10,34 @@ public class HelloWorld {
 
         System.out.println(sumGenerator(100, 10)); // 450
         System.out.println(sumGenerator(50, 4)); // 312
-        System.out.println(sumGenerator(1, 2)); // 0
+        System.out.println(sumGenerator(1, 2)); // 0*/
 
         System.out.println(wordSeparator("Hello", 1, 3)); // "el"
         System.out.println(wordSeparator("MagicalWord", 5, 11)); // "alWord"
         System.out.println(wordSeparator("KuuliLennuTeeTunneliLuuk", -12, 5)); // ""
 
+    }
+
+    private static String helloStudent(String name) {
+        int nameLen = 6;
+        if (name.length() <= nameLen) {
+            return "Hello student " + name + ", whose name is " + name.length() + " characters long!";
+        } else {
+            return "Hello student " + name + ", whose name is too long!";
+        }
+    }
+    private static int sumGenerator(int termination, int increment) {
+        int result = 0;
+        for (int i = 0; i != termination; i += increment) {
+            result += i;
+        }
+        return result;
+    }
+    private static String wordSeparator(String str, int start, int end) {
+        if (start < 0 || end > str.length()) {
+            return "";
+        } else {
+            return str.substring(start, end);
+        }
     }
 }
