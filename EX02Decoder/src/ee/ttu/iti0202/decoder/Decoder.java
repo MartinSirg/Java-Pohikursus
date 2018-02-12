dee9ce09ad2eb284eee6dedd1c6904c116dd18c3
Martin <masirg@ttu.ee>
2018-02-12 19:10:04 +0200

    v1.2.2 HELP BONUS 2 style fixes

--- EX02Decoder/src/ee/ttu/iti0202/decoder/Decoder.java
+++ EX02Decoder/src/ee/ttu/iti0202/decoder/Decoder.java
@@ -40,7 +40,9 @@ public class Decoder {
         Boolean upperCase = false;
         message = message + "#"; // extra room for loop: index error prevention
 
-        if (message.length() == 0) { return ""; }
+        if (message.length() == 0) {
+            return "";
+        }
 
         for (int i = 0; i < message.length() - 1; i++) {
             State current;
@@ -55,21 +57,24 @@ public class Decoder {
                 current = State.SHORT_INDEX;
             }
 
-            switch (current) {
-                case LONG_INDEX:    int regexInt = regexer("^\\d[+]\\d([+]\\d)*", message.substring(i)); // "1+2+354+1+2" => 123
-
-                                    result.append(caseDecider(upperCase, dictionary.charAt(regexInt))); // append dict char to result
+            switch (current) {                              // "1+2+354+1+2" => 123
+                case LONG_INDEX:    int regexInt = regexer("^\\d[+]\\d([+]\\d)*", message.substring(i));
+                                    result.append(caseDecider(upperCase, dictionary.charAt(regexInt)));
                                     i =  i + (String.valueOf(regexInt).length() - 1)  * 2;  // (len - 1) * 2
                     break;
                 case SHORT_INDEX:   int dictIndex = Integer.parseInt(String.valueOf(message.charAt(i)));
                                     result.append(caseDecider(upperCase, dictionary.charAt(dictIndex)));
                     break;
-                default:    if (upperCase) { result.append(Character.toUpperCase(message.charAt(i)));
-                            } else { result.append(message.charAt(i));
-                }
+                default:    if (upperCase) {
+                                result.append(Character.toUpperCase(message.charAt(i)));
+                            } else {
+                                result.append(message.charAt(i));
+                            }
 
             }
-            if (upperCase) { upperCase = false;};
+            if (upperCase) {
+                upperCase = false;
+            }
         }
 
         return result.toString();
@@ -86,39 +91,36 @@ public class Decoder {
         Matcher regexMatcher = checkRegex.matcher(str);
 
         if (regexMatcher.find()) {
-            String bigNumStr = regexMatcher.group().replaceAll("\\+","");
+            String bigNumStr = regexMatcher.group().replaceAll("\\+", "");
             return Integer.parseInt(bigNumStr);
         }
         return 0; //mdea miks siin nii väga nõuab returni
     }
 
     private static char caseDecider(Boolean upperCase, char c) {
-        if (upperCase) { return Character.toUpperCase(c);
-        } else { return c;}
+        if (upperCase) {
+            return Character.toUpperCase(c);
+        } else {
+            return c;
+        }
     }
 
     public static void main(String[] args) {
         System.out.print("Enter dictionary: ");
         Scanner scanner = new Scanner(System.in);
-        String dictionary = scanner.nextLine(), message;
-        StringBuilder sb = new StringBuilder();
-
+        String dictionary = scanner.nextLine();
         Pattern checkRegex = Pattern.compile("([^\\\\n]*)");
-        Matcher regexMatcher;
 
         while (true) {
             System.out.print("Enter message: ");
-            regexMatcher = checkRegex.matcher(scanner.nextLine());
+            Matcher regexMatcher = checkRegex.matcher(scanner.nextLine());
 
             while (regexMatcher.find()) {
                 if (regexMatcher.group().length() != 0) {
                     System.out.println("Decoded message: " + decodeMessage(dictionary, regexMatcher.group()));
                 }
             }
-
-                //
         }
     }
 
 }
-
