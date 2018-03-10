2bd1ad66a785046b69cc3b4790a3ff556f876a2c
Martin <masirg@ttu.ee>
2018-03-10 15:53:37 +0200

    v 1 stiil

--- PR06Cars/src/ee/ttu/iti0202/cars/car/ElectricCar.java
+++ PR06Cars/src/ee/ttu/iti0202/cars/car/ElectricCar.java
@@ -2,7 +2,7 @@ package ee.ttu.iti0202.cars.car;
 
 import ee.ttu.iti0202.cars.driver.Driver;
 
-public class ElectricCar extends Car{
+public class ElectricCar extends Car {
 
     public ElectricCar(Driver driver) {
         super(driver);
