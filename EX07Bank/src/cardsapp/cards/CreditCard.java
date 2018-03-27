a666a6201bd8b15c66ecb8d9d88b7ed4e7547507
Martin <masirg@ttu.ee>
2018-03-27 18:08:04 +0300

    java fx EX07 v1.11 stiil

--- EX07Bank/src/cardsapp/cards/CreditCard.java
+++ EX07Bank/src/cardsapp/cards/CreditCard.java
@@ -5,7 +5,7 @@ import javafx.beans.property.StringProperty;
 class CreditCard extends Card {
     private StringProperty limit;
 
-    CreditCard(String cardNum, String owner, String limit){
+    CreditCard(String cardNum, String owner, String limit) {
         super(cardNum, owner, limit);
     }
 }
