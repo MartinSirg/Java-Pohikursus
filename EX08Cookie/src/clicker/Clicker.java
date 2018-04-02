ac45cdf8077ac9517e225f065e009bf3dda8a605
Martin <masirg@ttu.ee>
2018-04-02 21:13:41 +0300

    java fx EX08 v1

--- EX08Cookie/src/clicker/Clicker.java
+++ EX08Cookie/src/clicker/Clicker.java
@@ -1,16 +1,11 @@
 package clicker;
 
-import clicker.view.MainViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -28,9 +23,9 @@ public class Clicker extends Application {
             loader.setLocation(Clicker.class.getResource("view/mainview.fxml"));
             System.out.println(loader.getLocation());
             root = loader.load();
-            MainViewController controller = loader.getController();
             Scene currentScene = new Scene(root);
 
+            primaryStage.initStyle(StageStyle.UNDECORATED);
             primaryStage.setScene(currentScene);
             primaryStage.setResizable(false);
             primaryStage.show();
--- EX08Cookie/src/clicker/view/MainViewController.java
+++ EX08Cookie/src/clicker/view/MainViewController.java
@@ -8,54 +8,160 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
+
 
 
 public class MainViewController {
 
+    private static final int TIME = 5100, MAX_KOLHOOS = 50, FIFTY = 50, GULAG_PRICE = 200, GULAG_POWER = 10,
+            MAX_WIDTH = 200;
+
     @FXML private BorderPane root;
 
-    @FXML private Label currentCookies;
+    @FXML private TextField textField;
+
+    @FXML private Label currentPotatoesLabel;
     @FXML private Label currentShovels;
     @FXML private Label currentKolhoosid;
     @FXML private Label currentGulagTourists;
+    @FXML private Label shovelPriceLabel;
+    @FXML private Label kolhoosPriceLabel;
+    @FXML private Label gulagPriceLabel;
 
     @FXML private Button buyShovelbtn;
     @FXML private Button buyKolhoosbtn;
     @FXML private Button buyGulagbtn;
+    @FXML private Button hackButton;
+    @FXML private Button closeBtn;
+    @FXML private Button infoBtn;
 
     @FXML private ImageView stalinTato;
     @FXML private ImageView gulagImg;
     @FXML private ImageView shovelImg;
     @FXML private ImageView kolhoosImg;
-    private Timeline timeline1 = new Timeline();
+
+    private Timeline imageTimeline = new Timeline(), kolhoosTimeline = new Timeline(), gulagTimeline = new Timeline();
 
     private IntegerProperty potatoes = new SimpleIntegerProperty(0);
     private IntegerProperty shovelsCount = new SimpleIntegerProperty(1);
-    private IntegerProperty kolhooscount = new SimpleIntegerProperty(0);
+    private IntegerProperty kolhoosCount = new SimpleIntegerProperty(0);
     private IntegerProperty gulagCount = new SimpleIntegerProperty(0);
     private IntegerProperty shovelPrice = new SimpleIntegerProperty(10);
-    private IntegerProperty kolhoosPrice = new SimpleIntegerProperty(50);
-    private IntegerProperty gulagPrice = new SimpleIntegerProperty(1000);
+    private IntegerProperty kolhoosPrice = new SimpleIntegerProperty(FIFTY);
+    private IntegerProperty gulagPrice = new SimpleIntegerProperty(GULAG_PRICE);
+
 
     @FXML
     public void initialize() {
         stalinTato.setPickOnBounds(false);
-        currentCookies.setWrapText(true);
-        timeline1.setCycleCount(2);
-        timeline1.setAutoReverse(true);
 
+        // initial timeline configs
+        imageTimeline.setCycleCount(2);
+        imageTimeline.setAutoReverse(true);
+        kolhoosTimeline.setCycleCount(Animation.INDEFINITE);
+        gulagTimeline.setCycleCount(Animation.INDEFINITE);
+
+
+        //bind price labels to price properties
+        shovelPriceLabel.textProperty().bind(shovelPrice.asString());
+        kolhoosPriceLabel.textProperty().bind(kolhoosPrice.asString());
+        gulagPriceLabel.textProperty().bind(gulagPrice.asString());
+
+
+        // potato count && shovel count
+        currentPotatoesLabel.textProperty().bind(potatoes.asString("Kardulaid: %d"));
+        currentShovels.textProperty().bind(shovelsCount.asString("Labidad: %d"));
+
+
+        potatoes.addListener((observable, oldValue, newValue) -> changeBtnStates(newValue.intValue()));
+        //kolhoos count listener
+        kolhoosCount.addListener((observable, oldValue, newValue) -> {
+            if (newValue.intValue() == MAX_KOLHOOS) {
+                currentKolhoosid.setText(String.format("Vabatahtlikke: MAX %d", newValue.intValue()));
+            } else {
+                currentKolhoosid.setText(String.format("Vabatahtlikke: %d", newValue.intValue()));
+            }
+            updateKolhoosTimeline();
+        });
+
+        //gulag count listener
+        gulagCount.addListener((observable, oldValue, newValue) -> {
+            currentGulagTourists.setText(String.format("Gulagi kylalisi: %d", newValue.intValue()));
+            updateGulagTimeline();
+        });
+
+        textField.textProperty().addListener(observable -> {
+            if (textField.getText().equals("pepe")) {
+                hackButton.setVisible(true);
+            }
+        });
+    }
+
+    private void changeBtnStates(Integer potatoCount) {
+        // validate shovel button
+        if (potatoCount < shovelPrice.get()) {
+            buyShovelbtn.setDisable(true);
+        } else {
+            buyShovelbtn.setDisable(false);
+        }
+        // validate kolhoos button
+        if (potatoCount < kolhoosPrice.get()) {
+            buyKolhoosbtn.setDisable(true);
+        } else {
+            buyKolhoosbtn.setDisable(false);
+        }
+        // validate gulag button
+        if (potatoCount < gulagPrice.get()) {
+            buyGulagbtn.setDisable(true);
+        } else {
+            buyGulagbtn.setDisable(false);
+        }
+    }
+
+    private void updateGulagTimeline() {
+        if (gulagTimeline.getStatus() == Animation.Status.RUNNING) { // timeline running
+            gulagTimeline.stop();
+            gulagTimeline.getKeyFrames().remove(0);
+        }
+        gulagTimeline.getKeyFrames().add(new KeyFrame(
+                Duration.seconds(1),
+                event -> {
+                    potatoes.setValue(potatoes.add(GULAG_POWER * gulagCount.get()).intValue());
+                    System.out.println("Got " + GULAG_POWER * gulagCount.get() + " potatoes from gulag");
+                }
+        ));
+        gulagTimeline.play();
+    }
+
+    private void updateKolhoosTimeline() {
+        if (kolhoosTimeline.getStatus() == Animation.Status.RUNNING) { // timeline running
+            kolhoosTimeline.stop();
+            kolhoosTimeline.getKeyFrames().remove(0);
+        }
+        kolhoosTimeline.getKeyFrames().add(new KeyFrame(
+                Duration.millis(TIME - 100 * kolhoosCount.get()),
+                event -> {
+                    potatoes.setValue(potatoes.add(shovelsCount).intValue());
+                    System.out.println(String.format("Got %d potatoes from kolhoos. Currently outputs every %d ms.",
+                            shovelsCount.get(), TIME - (100 * kolhoosCount.get())));
+                }
+        ));
+        kolhoosTimeline.play();
     }
 
     @FXML
     public void pressedStalin() {
         stalinTato.setCursor(Cursor.CLOSED_HAND);
-        potatoes.set(potatoes.getValue() + shovelsCount.get());
-        currentCookies.setText(String.format("Kardulaid: %d", potatoes.get()));
+        potatoes.set(potatoes.add(shovelsCount).intValue());
+        System.out.println(String.format("Got %d potatos by clicking on StalinTato.", shovelsCount.get()));
     }
 
     @FXML
@@ -65,26 +171,62 @@ public class MainViewController {
 
     @FXML
     public void buyShovelBtnPressed() {
-        fadeImage(shovelImg);
+        if (potatoes.get() >= shovelPrice.get()) {
+            potatoes.set(potatoes.subtract(shovelPrice).intValue());
+            shovelsCount.set(shovelsCount.add(1).intValue());
+            shovelPrice.set(shovelPrice.add(10).intValue());
+            fadeImage(shovelImg);
+        }
     }
 
     @FXML
     public void buyKolhoosBtnPressed() {
-        fadeImage(kolhoosImg);
+        if (potatoes.get() >= kolhoosPrice.get() && kolhoosCount.get() < MAX_KOLHOOS) {
+            potatoes.set(potatoes.subtract(kolhoosPrice).intValue());
+            kolhoosCount.set(kolhoosCount.add(1).intValue());
+            kolhoosPrice.set(kolhoosPrice.add(FIFTY).intValue());
+            fadeImage(kolhoosImg);
+        }
     }
 
     @FXML
     public void buyGulagBtnPressed() {
-        fadeImage(gulagImg);
+        if (potatoes.get() >= gulagPrice.get()) {
+            potatoes.set(potatoes.subtract(gulagPrice).intValue());
+            gulagCount.set(gulagCount.add(1).intValue());
+            gulagPrice.set(gulagPrice.add(GULAG_PRICE).intValue());
+            fadeImage(gulagImg);
+        }
     }
 
     private void fadeImage(ImageView image) {
-        if (!(timeline1.getStatus() == Animation.Status.RUNNING)) {
+        if (!(imageTimeline.getStatus() == Animation.Status.RUNNING)) {
             KeyValue opacity = new KeyValue(image.opacityProperty(), 1);
             KeyFrame frame = new KeyFrame(Duration.seconds(2), opacity);
-            timeline1.getKeyFrames().add(frame);
-            timeline1.play();
-            timeline1.setOnFinished(event -> timeline1.getKeyFrames().remove(frame));
+            imageTimeline.getKeyFrames().add(frame);
+            imageTimeline.play();
+            imageTimeline.setOnFinished(event -> imageTimeline.getKeyFrames().remove(frame));
         }
+    }
+
+    @FXML
+    public void hackBtnPressed() {
+        potatoes.setValue(potatoes.add(1000).intValue());
+    }
+
+    @FXML
+    private void closeBtnPressed() {
+        Stage stage = (Stage) closeBtn.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void infoBtnPressed() {
+        BorderPane info = new BorderPane();
+        info.setCenter(new Label("Made by Martin Sirg. 2. April 2017"));
+        Scene scene = new Scene(info, MAX_WIDTH, 100);
+        Stage newWindow = new Stage();
+        newWindow.setScene(scene);
+        newWindow.show();
     }
 }
