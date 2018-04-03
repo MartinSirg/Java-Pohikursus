c603ef3ce92094eba91605f4fefe92cea1a2f8db
Martin <masirg@ttu.ee>
2018-04-03 22:53:05 +0300

    java fx PR09 style

--- PR09TypeGame/src/typegame/Main.java
+++ PR09TypeGame/src/typegame/Main.java
@@ -3,12 +3,10 @@ package typegame;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import typegame.view.OverviewController;
 
-import java.io.IOException;
 
 public class Main extends Application {
     private static final int WIDTH = 1200, HEIGHT = 750;
