85b9293481a98e21a895a05c16f79e8f6d31b0f7
Martin <masirg@ttu.ee>
2018-04-19 18:22:23 +0300

    recSum EX

---
+++ EX11Rec/src/ee/ttu/iti0202/rec/Rec.java
@@ -0,0 +1,64 @@
+package ee.ttu.iti0202.rec;
+import java.util.List;
+
+public class Rec {
+    public static void main(String[] args) {
+
+
+        System.out.println(maxGrowth(List.of(1, 2, 3)));
+        System.out.println(maxGrowth(List.of(3, 2, 1)));
+        System.out.println(maxGrowth(List.of(1, 2, 3, 1, 2, 3, 4)));
+        System.out.println(maxGrowth(List.of(1, 2, 3, 3, 1, 5, 6)));
+    }
+
+    public static int maxElement(List<Integer> nums) {
+        if (nums.size() == 1) {
+            return nums.get(0);
+        } else {
+            int a = maxElement(nums.subList(1, nums.size()));
+            if (a > nums.get(0)) {
+                return a;
+            } else {
+                return nums.get(0);
+            }
+        }
+    }
+
+    public static int maxElement(List<Integer> nums, int max) {
+        if (nums.size() == 1) {
+            if (nums.get(0) > max) {
+                return nums.get(0);
+            } else {
+                return max;
+            }
+        } else {
+            if (nums.get(0) > max) {
+                return maxElement(nums.subList(1, nums.size()), nums.get(0));
+            } else {
+                return maxElement(nums.subList(1,nums.size()), max);
+            }
+        }
+    }
+
+    public static int maxGrowth(List<Integer> nums) {
+        return maxGrowth(nums, 1, 1);
+    }
+
+    public static int maxGrowth(List<Integer> nums, int currentStreak, int longest) {
+        if (nums.size() < 2) { // if list smaller than 2 is presented---------------------------------------------------
+            return nums.size();
+        } else if (nums.size() == 2) { // ending condition--------------------------------------------------------------
+            if (nums.get(0) + 1 == nums.get(1)) currentStreak += 1;
+            if (currentStreak > longest) longest = currentStreak;
+            return longest;
+        } else { // recursion conditions--------------------------------------------------------------------------------
+            if (nums.get(0) + 1 == nums.get(1)) {                                                    // streak continues
+                if (currentStreak + 1 > longest) longest = currentStreak + 1;
+                return maxGrowth(nums.subList(1, nums.size()), currentStreak + 1, longest);
+            } else {                                                                          // streak doesn't continue
+                return maxGrowth(nums.subList(1, nums.size()), 1, longest);
+            }
+        }
+    }
+
+}
