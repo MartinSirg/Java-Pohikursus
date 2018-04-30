8639e121790d24d30ca3e2d8bd6d7cc68f39f6d7
Martin <masirg@ttu.ee>
2018-04-30 17:04:19 +0300

    aprill

--- April/src/April.java
+++ April/src/April.java
@@ -3,7 +3,7 @@ public class April {
     public static String answer(int question) {
         switch (question) {
             case 0: return "10";
-            case 2: return "git reset HEAD";
+            case 2: return "git commit --amend";
             default: return "";
         }
     }
