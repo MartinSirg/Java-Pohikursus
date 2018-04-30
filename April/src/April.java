84ff13242ce48b3300437179aedf3fce02683d7e
Martin <masirg@ttu.ee>
2018-04-30 16:59:52 +0300

    aprill

--- April/src/April.java
+++ April/src/April.java
@@ -3,7 +3,7 @@ public class April {
     public static String answer(int question) {
         switch (question) {
             case 0: return "10";
-            case 2: return "git checkout HEAD";
+            case 2: return "git reset HEAD";
             default: return "";
         }
     }
@@ -11,13 +11,11 @@ public class April {
     public static int x(int a) {
         int result = 0;
         if (a < 0) {
-            System.out.println("-");
             for (int i = 0; i > a - 1; i--) {
                 System.out.println(result);
                 result += i;
             }
         } else {
-            System.out.println("+");
             for (int i = 0; i < a + 1; i++) {
                 result += i;
             }
