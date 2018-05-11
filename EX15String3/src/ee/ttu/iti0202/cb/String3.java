e664858b1480fc1b1b95a98324fda4a59a038cd1
masirg <masirg@ttu.ee>
2018-05-12 02:21:44 +0300

    cb

--- EX15String3/src/ee/ttu/iti0202/cb/String3.java
+++ EX15String3/src/ee/ttu/iti0202/cb/String3.java
@@ -7,7 +7,7 @@ public class String3 {
     public int countYZ(String str) {
         List<String> list = new ArrayList<>();
         int endOfLast = 0;
-        for(int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             if (!Character.isLetter(str.charAt(i))) {
                 list.add(str.substring(endOfLast, i));
                 endOfLast = i + 1;
@@ -15,10 +15,10 @@ public class String3 {
         }
         list.add(str.substring(endOfLast));
         int count = 0;
-        for(String word: list) {
+        for (String word: list) {
             if (word.length() != 0) {
                 char lastChar = word.toLowerCase().charAt(word.length() - 1);
-                if ( lastChar == 'y' || lastChar == 'z') count++;
+                if (lastChar == 'y' || lastChar == 'z') count++;
             }
         }
         return count;
@@ -49,8 +49,8 @@ public class String3 {
     public int countTriple(String str) {
         int count = 0;
         for(int i = 0; i < str.length() - 2; i++) {
-            if(str.charAt(i) == str.charAt(i + 1) &&
-                    str.charAt(i) == str.charAt(i + 2)) {
+            if(str.charAt(i) == str.charAt(i + 1)
+                    && str.charAt(i) == str.charAt(i + 2)) {
                 count++;
             }
         }
@@ -70,8 +70,8 @@ public class String3 {
     public String sameEnds(String string) {
         String result = "";
         for (int i = 1; i < (string.length() / 2) + 1; i++) {
-            if (string.substring(0,i).equals(string.substring(string.length() - i))) {
-                result = string.substring(0,i);
+            if (string.substring(0, i).equals(string.substring(string.length() - i))) {
+                result = string.substring(0, i);
             }
         }
         return result;
@@ -79,7 +79,7 @@ public class String3 {
 
     public String mirrorEnds(String string) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < string.length(); i++) {
+        for (int i = 0; i < string.length(); i++) {
             if (string.charAt(i) == string.charAt(string.length() - i - 1)) {
                 sb.append(string.charAt(i));
             } else {
@@ -92,13 +92,13 @@ public class String3 {
     public int maxBlock(String str) {
         if (str.length() == 0) return 0;
         int currentMax = 1;
-        for(char c: str.toCharArray()) {
-            for(int i = 1; i < str.length(); i++) {
+        for (char c: str.toCharArray()) {
+            for (int i = 1; i < str.length(); i++) {
                 StringBuilder sb = new StringBuilder();
                 for(int j = 0; j < i; j++) {
                     sb.append(c);
                 }
-                if(str.contains(sb.toString()) && sb.toString().length() > currentMax){
+                if (str.contains(sb.toString()) && sb.toString().length() > currentMax) {
                     currentMax = sb.toString().length();
                 }
             }
